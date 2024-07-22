fd6dbc1dc9cf8d1256d6ce34c26159e566262976
MayDomine <1583143678@qq.com>
2024-07-22 14:58:49 +0800

    view lastest telescope result

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,89 +2,73 @@ require "nvchad.mappings"
 
 -- add yours here
 
-local nore = { noremap = true, silent = true }
+local nore = { noremap = true , silent = true}
 local map = vim.keymap.set
 local nmap = vim.api.nvim_set_keymap
 local function opts(desc)
-  return { desc = desc }
+  return {desc = desc }
 end
 map("n", "<C-a>", "gg<S-v>G")
-map("n", "<leader>b", "")
 map("n", "<leader>v", "", opts "")
 map("n", "<leader>n", "", opts "")
-map("n", "<C-i>", "<S-Tab>", { remap = true })
+map("n", "<C-i>", "<S-Tab>", {remap=true})
 
-map({ "s", "i" }, "<Tab>", function()
+map({"s","i"}, "<Tab>", function ()
   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
 end, opts "luasnip jump-next/expand")
 
-map({ "s", "i" }, "<S-Tab>", function()
+map({"s","i"}, "<S-Tab>", function ()
   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
 end, opts "luasnip jump-prev")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 nmap("n", "<esc>", "<esc>", nore)
+map("n", "<leader>gl", "<cmd>Gitsigns blame_line<CR>", opts "Check Blame Line")
 map("n", "<leader>gg", "<cmd>Git<CR>", opts "Open Fugitive")
 map("n", "<leader>;", "<cmd>Git<CR>", opts "Open Fugitive")
 map("n", "<leader>gd", ":DiffviewOpen <C-R><C-W>", opts "Open Diffview")
 map("n", "<leader>gm", "<cmd>Merginal<CR>", opts "Open Merginal")
 map("v", "<leader>do", "<cmd>'<,'>diffget<CR>")
-map("n", "<leader>gs", "<cmd>Git status<CR>", opts "Search Git status")
+map("n", "<leader>gs", "<cmd>Telescope git_status<CR>", opts "Search Git status")
 map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", opts "Search Git Commits")
-map("n", "<leader>gb", "<cmd>Telescope git_branches<CR>", opts "Search Git Branches")
+map("n", "<leader>gab", "<cmd>Telescope git_branches<CR>", opts "Search Git Branches")
 map("n", "<leader>gf", "<cmd>Telescope git_bcommits<CR>", opts "Search Git Branches Related Current buffer")
 map("n", "<leader>gr", "<cmd>Telescope git_bcommits_range<CR>", opts "Git Commits Related Current Lines")
 map("n", "<leader>i", "<cmd>Navbuddy<CR>", opts "Navbuddy")
-map("n", "<leader>ar", "<cmd>ARsyncUp<CR>", opts "Sync Up")
-map("n", "<leader>ad", "<cmd>ARsyncDown<CR>", opts "Sync Down")
+
 
 map("n", "<leader>vs", "<cmd>sp<CR>", opts "Split Horizontal")
 map("n", "<leader>vv", "<cmd>vsp<CR>", opts "Split Vertical")
 -- map '+m for m
-nmap("n", "'m", "m", { noremap = true })
-map({ "t" }, "<C-w>", "<C-\\><C-n><C-w>", { noremap = true })
-map({ "n", "t" }, "<C-j>", function()
-  require("nvchad.term").toggle { pos = "sp", id = "apple-toggleTerm", size = 0.3 }
+nmap("n", "'m", "m", {noremap = true})
+map({"t"}, "<C-w>", "<C-\\><C-n><C-w>", {noremap = true})
+map({"n", "t"}, "<C-j>", function()
+  require("nvchad.term").toggle({ pos = "sp", id ='apple-toggleTerm' , size = 0.3})
 end, { desc = "Terminal Toggle " })
 
-map({ "n", "t" }, "<C-p>", "<cmd>wincmd p<CR>", { desc = "Terminal Toggle " })
+map({"n", "t"}, "<C-p>", "<cmd>wincmd p<CR>", { desc = "Terminal Toggle " })
 
-map({ "n", "t" }, "<C-k>", function()
-  require("nvchad.term").toggle { pos = "vsp", id = "apple-vstoggleTerm", size = 0.3 }
+map({"n", "t"}, "<C-k>", function()
+require("nvchad.term").toggle({ pos = "vsp", id ='apple-vstoggleTerm' , size = 0.3})
 end, { desc = "Terminal Toggle Vertical" })
 
 map("n", "<leader>tf", function()
-  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
 end, { desc = "Terminal toggle Floating term" })
 map("n", "<leader>ft", "<cmd>Telescope terms<CR>", { desc = "Search Terminal" })
-map("n", "<leader>db", "<cmd>Gitsigns toggle_current_line_blame<CR>", opts "Toggle Current Line Blame")
-map("n", "<leader>dv", function()
-  require("nvim-tree.api").tree.close_in_this_tab()
-  vim.cmd "windo diffthis"
-end, opts "Open diffview between all windowss")
+map('n', '<leader>db', "<cmd>Gitsigns toggle_current_line_blame<CR>", opts "Toggle Current Line Blame")
 
-map("n", "<leader>do", "<cmd>windo diffoff<CR>", opts "Close diffview in all window ")
+map('n', '<leader>fw', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", opts "Live Grep Args")
+map('n', '<leader>fl', "<cmd>Telescope resume<CR>", opts "Resume Last Telescope")
 
-map(
-  "n",
-  "<leader>fw",
-  ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-  opts "Live Grep Args"
-)
-
-local live_grep_args_shortcuts = require "telescope-live-grep-args.shortcuts"
-map(
-  "n",
-  "<leader>fz",
-  ":lua require('telescope').extensions.live_grep_args.live_grep_args({search_dirs={vim.fn.expand(\"%\")}})<CR>",
-  opts "Live grep current buffer"
-)
-map({ "n", "v" }, "<leader>fc", live_grep_args_shortcuts.grep_word_under_cursor, opts "Live grep word")
-map({ "n", "v" }, "<leader>fC", live_grep_args_shortcuts.grep_word_under_cursor_current_buffer, opts "Live grep word")
+local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+map("n", "<leader>fz", ":lua require('telescope').extensions.live_grep_args.live_grep_args({search_dirs={vim.fn.expand(\"%\")}})<CR>", opts "Live grep current buffer")
+map({"n","v"}, "<leader>fc", live_grep_args_shortcuts.grep_word_under_cursor, opts "Live grep word")
+map({"n","v"}, "<leader>fC", live_grep_args_shortcuts.grep_word_under_cursor_current_buffer, opts "Live grep word")
 
 map("n", "<leader>fn", "<cmd>Telescope notify<CR>", opts "Search Noice history")
-map("n", "<leader>fr", "<cmd>Telescope resume<CR>", opts "Last Tele history")
-map("n", "<leader>fk", "<cmd>Telescope keymaps <CR>", opts "Search Keymaps")
+map("n", "<leader>fm", "<cmd>Telescope keymaps <CR>", opts "Search Keymaps")
+map("n", "<leader>fh", "<cmd>Telescope command_history<CR>", opts "Search command_history")
 map("n", "<leader>ld", vim.diagnostic.setloclist, opts "Lsp Loclist")
 map("n", "<leader>qd", "<cmd>bdelete<CR>", opts "Delete Buffer")
 map("n", "<leader>qa", "<cmd>SessionSave<CR><cmd>bdelete<CR><cmd>wqa<CR>", opts "Exit (wqa) and SessionSave")
@@ -95,36 +79,28 @@ map("n", "<leader>fd", "<cmd>Telescope command_history<CR>", opts "Close Current
 -- lsp mappings
 vim.g.diagnostics_active = true
 function _G.toggle_diagnostics()
-  if vim.g.diagnostics_active then
-    vim.g.diagnostics_active = false
-    vim.diagnostic.reset()
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
-    vim.notify("Diagnostics are now off", vim.log.levels.INFO, { title = "Diagnostics" })
-  else
-    vim.g.diagnostics_active = true
-    vim.diagnostic.enable()
-    vim.diagnostic.show()
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+if vim.g.diagnostics_active then
+  vim.g.diagnostics_active = false
+  vim.diagnostic.reset()
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+  vim.notify("Diagnostics are now off", vim.log.levels.INFO, { title = "Diagnostics" })
+else
+  vim.g.diagnostics_active = true
+  vim.diagnostic.enable()
+  vim.diagnostic.show()
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = false,
       signs = true,
       underline = true,
       update_in_insert = true,
-    })
-    vim.notify("Diagnostics are now no", vim.log.levels.INFO, { title = "Diagnostics" })
-  end
+    }
+  )
+  vim.notify("Diagnostics are now no", vim.log.levels.INFO, { title = "Diagnostics" })
 end
-map(
-  "n",
-  "<leader>td",
-  ":call v:lua.toggle_diagnostics()<CR>",
-  { noremap = true, silent = true, desc = "Toggle Diagnostics" }
-)
-map(
-  "v",
-  "<C-r>",
-  'y<ESC><cmd>lua require(\'telescope\').extensions.live_grep_args.live_grep_args({search_dirs={vim.fn.expand("%")}})<CR><C-R>"',
-  { remap = true }
-)
+end
+map('n', '<leader>td', ':call v:lua.toggle_diagnostics()<CR>',  {noremap = true, silent = true, desc = "Toggle Diagnostics"})
+map('v', '<C-r>', "y<ESC><cmd>lua require('telescope').extensions.live_grep_args.live_grep_args({search_dirs={vim.fn.expand(\"%\")}})<CR><C-R>\"", {remap=true})
 -- :cdo %s//g<left
 
 -- map('n', '<leader>gs', gs.stage_hunk)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,14 +8,10 @@ return {
   end,
   config = function(_, opts)
     dofile(vim.g.base46_cache .. "telescope")
-        local telescope = require "telescope"
-        local lga_actions = require "telescope-live-grep-args.actions"
-        local open_with_trouble = function(buf, sub_opts)
-          sub_opts = {}
-          sub_opts['focus']=true
-          require("trouble.sources.telescope").open(buf, sub_opts)
-        end
-        opts['extensions']['live_grep_args'] = {
+    local telescope = require "telescope"
+    local lga_actions = require "telescope-live-grep-args.actions"
+
+    opts['extensions']['live_grep_args'] = {
           auto_quoting = true, -- enable/disable auto-quoting
           -- define mappings, e.g.
           mappings = { -- extend mappings
@@ -26,15 +22,9 @@ return {
             },
 
           },
-        }
-        opts['defaults']['mappings'] = {
-          i = { ["<c-j>"] = open_with_trouble },
-          n = { ["<c-j>"] = open_with_trouble },
-        }
-        opts['defaults']['cache_picker'] = {
-          num_pickers = 100,
-        }
     telescope.setup(opts)
+
+  }
 
     -- load extensions
     for _, ext in ipairs(opts.extensions_list) do
