a0a9cfc1ae698865b62bcfcee0b8e83a38de03c5
MayDomine <1583143678@qq.com>
2024-07-25 22:37:57 +0800

    session.lua and window-picker

---
+++ lua/plugins/session.lua
@@ -0,0 +1,20 @@
+
+  return {
+   'rmagatti/auto-session',
+    lazy=false,
+    config = function()
+    local function open_tree()
+      require('nvim-tree.api').tree.toggle(false, true)
+    end
+    require("auto-session").setup({
+      log_level = "error",
+      auto_session_suppress_dirs = {  "~/projects", "~/.config", "/.local/share/nvim"},
+      post_restore_cmds =  {open_tree},
+      pre_save_cmds = { "NvimTreeClose" },
+      session_lens = {
+        load_on_setup = false,
+        }
+    })
+    end,
+}
+
