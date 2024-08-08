d71860339a0dee68ec17573447094aa6916ff592
MayDomine <1583143678@qq.com>
2024-08-08 11:43:51 +0800

    smart split

---
+++ lua/plugins/resize.lua
@@ -0,0 +1,30 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  version = ">=1.0.0",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>mm",
+      mode = { "n" },
+      function()
+        require("smart-splits").start_resize_mode()
+      end,
+      desc = "Start Resize mode",
+    },
+  },
+  config = function()
+    require("smart-splits").setup {
+      resize_mode = {
+        silent = true,
+        hooks = {
+          on_enter = function()
+            vim.notify "Entering resize mode"
+          end,
+          on_leave = function()
+            vim.notify "Exiting resize mode, bye"
+          end,
+        },
+      },
+    }
+  end,
+}
