48cb64ce1af03485b66f1f25f539eec5d9ace4d4
MayDomine <1583143678@qq.com>
2024-08-09 18:45:42 +0800

    update conform config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,9 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    python = function(bufnr)
+      if require("conform").get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
+    json = { "fixjson" },
   },
+  -- css = { "prettier" },
+  -- html = { "prettier" },
 
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
