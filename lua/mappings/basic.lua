ad8d0e67a81a939b11019e48ada77c444e558084
MayDomine <1583143678@qq.com>
2024-08-30 13:05:22 +0800

    update replace keybindings

--- lua/mappings/basic.lua
+++ lua/mappings/basic.lua
@@ -1,13 +1,28 @@
 local map = vim.keymap.set
-map("n", "<C-u>", "<C-u>zz")
-map("n", "<C-d>", "<C-d>zz")
-map("n", "n", "nzz")
-map("n", "N", "Nzz")
 map("n", "<leader>rl", ":s/\\v", { desc = "search and replace on line" })
-map('n', '<leader>rc', ':%s/\\v',                            { desc = "search and replace in file" })
-map('v', '<leader>rl', ':s/\\%V',                 { desc = "Search only in visual selection using %V atom" })
-map('v', '<leader>rc', '"hy:%s/\\v<C-r>h//g<left><left>', { desc = "change selection" })
-map("i", "<c-b>", function()
+
+map("n", "<leader>rc", function()
+  local cword = vim.fn.expand("<cword>")
+  local cmd = ":%s/\\v" .. cword .. "/" .. cword
+  vim.api.nvim_feedkeys(cmd, "n", true)
+end, { desc = "Replace current buffer" })
+
+
+map("v", "<leader>rl", ":s/\\%V", { desc = "Search only in visual selection using %V atom" })
+
+map("v", "<leader>rc", '"hy:%s/\\v<C-r>h//g<left><left>', { desc = "Replace current buffer" })
+
+map({"i"}, "<c-i>", function()
   require("telescope.builtin").registers()
 end, { noremap = true, silent = true, desc = "Show registers" })
+
+map({"n"}, "<leader>rg", function()
+  require("telescope.builtin").registers()
+end, { noremap = true, silent = true, desc = "Show registers" })
+
+map("n", "<leader>rC", function()
+  local cword = vim.fn.expand("<cword>")
+  local cmd = ":cdo s/" .. cword .. "/" .. cword
+  vim.api.nvim_feedkeys(cmd, "n", true)
+end, { desc = "Replace in quickfix list" })
 
