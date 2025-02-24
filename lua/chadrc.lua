544276a49591da7640f1967df77fb3bf1d4acc4e
MayDomine <1583143678@qq.com>
2025-02-24 22:30:29 +0800

    MayDomine's nvim configuration file 1.0.0

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,7 +5,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "gruvchad",
+  theme = "nightfox",
   telescope = { style = "bordered" }, -- borderless / bordered
   transparency = true,
   hl_override = {
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,6 +5,13 @@ return {
     opts = {},
     config = function()
       require("noice").setup {
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
         lsp = {
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -16,7 +23,7 @@ return {
             enabled = false,
           },
           signature = {
-            enabled = false,
+            enabled = true,
             silent = true,
           },
         },
