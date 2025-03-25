7c77a38ff4970dbe1eac062d39754bb061b56dc5
MayDomine <1583143678@qq.com>
2025-03-25 15:29:01 +0800

    simpify arsync conf

--- lua/plugins/vim-arsync.lua
+++ lua/plugins/vim-arsync.lua
@@ -2,28 +2,37 @@ return {
   "https://github.com/MayDomine/arsync.nvim.git",
   build = ":UpdateRemotePlugins",
   lazy = true,
-  dependencies = {
-    "folke/snacks.nvim",
+  dependencies = { "folke/snacks.nvim" },
+  cmd = {
+    "ARSyncShow",
+    "ARSyncToggle",
+    "ARSyncEnable",
+    "ARSyncDisable",
+    "ARSync",
+    "ARSyncProj",
+    "ARSyncDown",
+    "ARSyncDownProj",
+    "ARSyncDelete",
+    "ARSyncCleanSftp",
+    "ARCreate",
   },
   keys = {
-    "<leader>ar",
-    "<leader>as",
-    "<leader>ad",
-    "<leader>ac",
-    "<leader>aw",
-    "<leader>ax"
+    { "<leader>ar", "<cmd>ARSyncProj<CR>", desc = "ARSyncUpProj To Remote" },
+    { "<leader>as", "<cmd>ARSyncShow<CR>", desc = "ARSyncShow" },
+    { "<leader>ad", "<cmd>ARSyncDown<CR>", desc = "ARSyncDown From Remote" },
+    { "<leader>aD", "<cmd>ARSyncDownProj<CR>", desc = "ARSyncDownProj From Remote" },
+    { "<leader>ac", "<cmd>ARCreate<CR>", desc = "ARSyncUp Config Create" },
+    { "<leader>aw", "<cmd>ARSync<CR>", desc = "ARSyncUp file" },
+    {
+      "<leader>ax",
+      function()
+        require("arsync").cleanup()
+      end,
+      desc = "ARSyncUp file",
+    },
   },
   event = "BufWritePre",
   config = function()
-    vim.keymap.set("n", "<leader>ar", "<cmd>ARSyncProj<CR>", { desc = "ARSyncUpProj To Remote" })
-    vim.keymap.set("n", "<leader>as", "<cmd>ARSyncShow<CR>", { desc = "ARSyncShow" })
-    vim.keymap.set("n", "<leader>ad", "<cmd>ARSyncDownProj<CR>", { desc = "ARSyncDownProj From Remote" })
-    vim.keymap.set("n", "<leader>ac", "<cmd>ARCreate<CR>", { desc = "ARSyncUp Config Create" })
-    vim.keymap.set("n", "<leader>aw", "<cmd>ARSync<CR>", { desc = "ARSyncUp file" })
-    vim.keymap.set("n", "<leader>ax", function ()
-      require("arsync").cleanup(x)
-    end, { desc = "ARSyncUp file" })
     require("arsync").setup()
   end,
 }
-
