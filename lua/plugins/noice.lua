7a03455d3d2c601a13145a02df35bad17c19f074
MayDomine <1583143678@qq.com>
2025-05-19 10:47:13 +0800

    feat(noice): add macro recording notifications

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,30 @@ return {
     lazy = false,
     opts = {},
     config = function()
+      vim.api.nvim_create_autocmd("RecordingEnter", {
+        callback = function()
+          local msg = string.format("Recording Mode for:  %s", vim.fn.reg_recording())
+          _MACRO_RECORDING_STATUS = true
+          vim.notify(msg, vim.log.levels.INFO, {
+            title = "Macro Recording",
+            keep = function()
+              return _MACRO_RECORDING_STATUS
+            end,
+          })
+        end,
+        group = vim.api.nvim_create_augroup("NoiceMacroNotfication", { clear = true }),
+      })
+
+      vim.api.nvim_create_autocmd("RecordingLeave", {
+        callback = function()
+          _MACRO_RECORDING_STATUS = false
+          vim.notify("Record Finished!", vim.log.levels.INFO, {
+            title = "Macro Recording End",
+            timeout = 2000,
+          })
+        end,
+        group = vim.api.nvim_create_augroup("NoiceMacroNotficationDismiss", { clear = true }),
+      })
       require("noice").setup {
         presets = {
           bottom_search = false, -- use a classic bottom cmdline for search
