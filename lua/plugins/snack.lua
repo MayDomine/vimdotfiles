a02c3a4ef357096fecc09b7d050b5b54bac1bb94
MayDomine <1583143678@qq.com>
2026-03-25 16:51:55 +0800

    add snacks-gh keybinding

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -170,6 +170,10 @@ return {
       end,
       desc = "Delete Buffer",
     },
+      { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+      { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+      { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+      { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
     {
       "<leader>ll",
       function()
